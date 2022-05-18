64adbe3f42e2fa43c278b23a2214be6e514d8a0e
ajhall1979 <105619625+ajhall1979@users.noreply.github.com>
2022-05-18 09:56:49 -0700

    Add files via upload

---
+++ pizza_ordering_chat_box.py
@@ -0,0 +1,15 @@
+print("Hello, my name is Alex your virtual assistant. I will help you order pizza!")
+print("I am going to ask you a few questions.  After typing an answer, pres enter.")
+  print("/n")
+ if userName(). == "ARTHUR HALL":
+  print("\nMY creator,"+ username+ ". Pleasure to serve you!")
+ else: print("Hello, " + userName +". Nice to meet you!")
+  size=("\nWhat size do you want? Enter small, medium or large: ")
+  flavor = ("\nEnter the flavor of pizza: ")
+  crustType = input("\nHow many of these do you want to order:")
+  quanity = input("\nWhat type of crust you want to order:"
+  method = input("\nIs this carry out or delivery:")
+ 
+if size.lower() == "small": pizzaCost = 8.99
+elif size.lower() == "medium": pizzaCost = 14.99
+elif size.lower() == "large": pizzaCost =17.99
